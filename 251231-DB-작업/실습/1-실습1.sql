682e2ca57f7e7097cf2fc091ec66f15d5e5e4824
lsy3709 <lsy3709@naver.com>
2025-12-31 16:31:51 +0900

    7교시, DISTINCT 기본 예시

--- 251231-DB-작업/실습/1-실습1.sql
+++ 251231-DB-작업/실습/1-실습1.sql
@@ -3,16 +3,16 @@
 -- 급여가 3000 이상인 직원만 조회하시오.
 -- 셀렉션 : 급여가 3000 이상
 -- 프로젝션 : 언급 없음 -> 모든 컬럼 
-SELECT * from emp where sal >= 3000; 
+SELECT * FROM EMP WHERE SAL >= 3000; 
 
 -- 2) 
 -- EMP 테이블에서 이름(ENAME), 급여(SAL), 부서번호(DEPTNO)만 조회하시오.  
 -- 셀렉션 : 조건 없음
 -- 프로젝션 : 이름(ENAME), 급여(SAL), 부서번호(DEPTNO)
-SELECT ENAME, SAL, DEPTNO from emp;
+SELECT ENAME, SAL, DEPTNO FROM EMP;
 
 -- 3) 
 -- 20번 부서의 사원 이름과 급여를 조회하시오.
 -- 셀렉션 : 20번 부서
 -- 프로젝션 : 사원 이름(ENAME), 급여(SAL) + 추가 부서번호 궁금해요.
-SELECT ENAME, SAL from emp where DEPTNO = 20; 
+SELECT ENAME, SAL FROM EMP WHERE DEPTNO = 20; 

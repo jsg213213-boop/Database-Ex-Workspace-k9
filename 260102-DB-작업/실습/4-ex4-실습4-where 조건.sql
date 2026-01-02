ed291d79eeeb0d7ddac3206029a2757ddda10a4f
lsy3709 <lsy3709@naver.com>
2026-01-02 14:18:32 +0900

    5교시, where 조건절 확인

---
+++ 260102-DB-작업/실습/4-ex4-실습4-where 조건.sql
@@ -0,0 +1,21 @@
+-- 실습4 
+-- 1) 급여(SAL)가 2000 이상인 사원만 조회하시오.  
+SELECT * FROM EMP WHERE SAL >= 2000;
+
+-- 2) 입사일(HIREDATE)이 '1981-02-20' 이후인 사원만 조회하시오. 
+-- 힌트)TO_DATE('1981-02-20', 'YYYY-MM-DD') 이용하기.
+
+-- 방법1)
+SELECT * FROM EMP 
+WHERE HIREDATE > TO_DATE('1981-02-20', 'YYYY-MM-DD');
+
+-- 방법2)
+SELECT * FROM EMP 
+WHERE HIREDATE > '1981-02-20'
+ORDER BY HIREDATE DESC;
+
+ 
+-- 3)부서번호가 10이 아닌 사원만 출력하시오.
+SELECT * FROM EMP WHERE DEPTNO != 10;
+-- 또는 
+SELECT * FROM EMP WHERE DEPTNO <> 10;
